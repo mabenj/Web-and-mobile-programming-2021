7b7f602fed5669a99bdc5950835955caf213515c
Marko Järvinen <mabeja@utu.fi>
2021-03-22 02:06:00 +0200

    Vaihda status bar väri

--- exercise4/App.js
+++ exercise4/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "react-native";
 
 import NotesList from "./components/NotesList";
 import AddNoteForm from "./components/AddNoteForm";
@@ -17,6 +18,7 @@ const headerOptions = {
 export default function App() {
 	return (
 		<NavigationContainer>
+			<StatusBar backgroundColor={headerOptions.headerStyle.backgroundColor} barStyle="light-content" />
 			<Stack.Navigator initialRouteName="Notes" screenOptions={headerOptions}>
 				<Stack.Screen name="Notes" component={NotesList} options={{ title: "Muistiinpanot" }} />
 				<Stack.Screen name="AddNote" component={AddNoteForm} options={{ title: "Uusi muistiinpano" }} />
